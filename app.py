86cd5d28a14f3587fdcaf5455b77b9cb455ce655
Amir Nejati <amir.n@rahgoshagroup.com>
2021-07-05 04:14:54 +0430

    rate limiting added

---
+++ app.py
@@ -0,0 +1,66 @@
+from typing import Tuple
+
+from fastapi.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import PlainTextResponse
+from fastapi import status
+from ratelimit import RateLimitMiddleware, Rule
+from ratelimit.auths import EmptyInformation
+from ratelimit.backends.redis import RedisBackend
+from ratelimit.types import Scope
+from redis.connection import ConnectionPool
+
+from config import Config
+
+
+class CustomRedisBackend(RedisBackend):
+    def __init__(self):
+        connection_kwargs = \
+            ConnectionPool.from_url(Config.REDIS_URL).connection_kwargs
+        options = dict(
+            (i, connection_kwargs.get(i)) for i in
+            ('host', 'port', 'db', 'password')
+        )
+        super().__init__(**options)
+
+
+async def client_ip(scope: Scope) -> Tuple[str, str]:
+    """
+    instead of default ratelimit.auths.ip.client_ip method
+
+    """
+    ip = None
+    if scope["client"]:
+        ip, _ = tuple(scope["client"])
+    for name, value in scope["headers"]:  # type: bytes, bytes
+        if name == b"x-real-ip":
+            ip = value.decode("utf8")
+    if ip is None:
+        raise EmptyInformation(scope)
+    return ip, 'default'
+
+
+def throttling_exception_handler(exc: Exception):
+    return PlainTextResponse(
+        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+        content="too many requests due to rate-limiting policy",
+    )
+
+
+middleware_list = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+    ),
+    Middleware(
+        RateLimitMiddleware,
+        authenticate=client_ip,
+        backend=CustomRedisBackend(),
+        config={
+            r"^/v1/connected/realtime": [Rule(minute=10, block_time=60)],
+            r"^/v1/connected/register": [Rule(minute=100, block_time=60)],
+        },
+        on_blocked=throttling_exception_handler
+    ),
+]
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 import modules.connectivity.api.rest.v1.view as connectivity_view_v1
 from helper.custom_renderer import CustomDataResponse
 from helper.custom_exc_handlers import exc_handlers
+from app import middleware_list
 
 
 app = FastAPI(
@@ -12,15 +12,14 @@ app = FastAPI(
     version="0.0.1",
     default_response_class=CustomDataResponse,
     exception_handlers=exc_handlers,
+    middleware=middleware_list,
 )
 
 app.include_router(router=connectivity_view_v1.router, prefix='/v1/connected')
-app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"])
 
 
 if __name__ == '__main__':
     import uvicorn
-    # asgi_app = SentryAsgiMiddleware(app)
     uvicorn.run(
         "main:app", host="127.0.0.1", port=8000,
         log_level="debug", reload=True, debug=True
